cf17fd22094c433a75b7d3b4b543cb62af51c982
shinmyatnoezin <u6530086@au.edu>
2024-08-22 16:47:19 +0700

    update the carTable.jsx and carTable.css

--- car-analytics-app/src/components/carTable.jsx
+++ car-analytics-app/src/components/carTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './stylesheets/carTable.css'; 
 
 const aggregateData = (cars) => {
@@ -29,112 +29,103 @@ const CarTable = ({ cars }) => {
   const aggregatedData = aggregateData(cars);
   const [selectedModel, setSelectedModel] = useState(null);
   const [selectedBrand, setSelectedBrand] = useState(null);
+  const [favorites, setFavorites] = useState(() => {
+    const storedFavorites = localStorage.getItem('favorites');
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
 
-  const [favorites, setFavorites] = useState({});
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }, [favorites]);
 
   const handleModelClick = (brand, model) => {
     setSelectedBrand(brand);
     setSelectedModel(model);
   };
 
-
   const toggleFavorite = (car) => {
     setFavorites((prevFavorites) => {
-      const carId = car.Img100; // Assuming the image URL is unique
-      if (prevFavorites[carId]) {
-        const { [carId]: removed, ...rest } = prevFavorites;
-        return rest;
+      const carId = car.Cid;
+      if (prevFavorites.includes(carId)) {
+        return prevFavorites.filter(id => id !== carId);
       } else {
-        return { ...prevFavorites, [carId]: car };
+        return [...prevFavorites, carId];
       }
     });
   };
 
   const isFavorite = (car) => {
-    return !!favorites[car.Img100];
+    return favorites.includes(car.Cid);
   };
 
-
   return (
-    <div className="car-table-container">
+    <div className={`car-table-container ${selectedModel && selectedBrand ? 'show-details' : ''}`}>
       <div className="car-table-wrapper">
-        <h2>Car Table</h2>
-        <div style={{ height: '500px', overflowY: 'scroll' }}>
-          <table className="car-table">
-            <thead>
-              <tr>
-                <th>Brand</th>
-                <th>Model</th>
-                <th>Number of Cars</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(aggregatedData).map((brand, brandIndex) => (
-                <React.Fragment key={brand}>
-                  <tr className="brand-row">
-                    <td className='brands-text'>{brand}</td>
+        <h2 className="car-table-header">Car Table</h2>
+        <table className="car-table">
+          <thead>
+            <tr>
+              <th>Brand</th>
+              <th>Model</th>
+              <th>Number of Cars</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(aggregatedData).map((brand, brandIndex) => (
+              <React.Fragment key={brand}>
+                <tr className="brand-row">
+                  <td className='brands-text'>{brand}</td>
+                  <td></td>
+                  <td>{aggregatedData[brand].totalCars}</td>
+                </tr>
+                {Object.keys(aggregatedData[brand].models).map((model, modelIndex) => (
+                  <tr key={model} onClick={() => handleModelClick(brand, model)} className={`clickable-row ${modelIndex === 0 ? 'first-model-row' : ''}`}>
                     <td></td>
-                    <td>{aggregatedData[brand].totalCars}</td>
+                    <td>{model}</td>
+                    <td>{aggregatedData[brand].models[model].count}</td>
                   </tr>
-                  {Object.keys(aggregatedData[brand].models).map((model, modelIndex) => (
-                    <tr key={model} onClick={() => handleModelClick(brand, model)} className={`clickable-row ${modelIndex === 0 ? 'first-model-row' : ''}`}>
-                      <td></td>
-                      <td>{model}</td>
-                      <td>{aggregatedData[brand].models[model].count}</td>
-                    </tr>
-                  ))}
-                </React.Fragment>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                ))}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
       </div>
       <div className={`details-container ${selectedModel && selectedBrand ? '' : 'hidden'}`}>
         {selectedModel && selectedBrand && (
           <div>
-            <h2>{selectedBrand} {selectedModel} Details</h2>
-            <div style={{ height: '400px', overflowY: 'scroll' }}>
-              <table className="car-table">
-                <thead>
-                  <tr>
-                    <th>Image</th>
-                    <th>Price</th>
-                    <th>Year</th>
-                    <th>Province</th>
-                    <th>Status</th>
-
-                    <th>Favorite</th>
-
-                  </tr>
-                </thead>
-                <tbody>
-                  {aggregatedData[selectedBrand].models[selectedModel].cars.map((car, index) => {
-
-
-                    console.log('Image URL:', car.Img100);
-
-                    return (
-                      <tr key={index}>
-                        <td>
-                          <img src={car.Img100} alt="Car" className="car-image" onError={(e) => { e.target.onerror = null; e.target.src = 'fallback-image-url.jpg'; }} />
-                        </td>
-                        <td>{car.Prc}</td>
-                        <td>{car.Yr}</td>
-                        <td>{car.Province}</td>
-                        <td>{car.Status}</td>
-
-                        <td>
-                          <button className={`favorite-button`} onClick={() => toggleFavorite(car)}>
-                            {isFavorite(car) ? '★' : '☆'}
-                          </button>
-                        </td>
-
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+            <h2 className="details-header">{selectedBrand} {selectedModel} Details</h2>
+            <table className="car-table">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Price</th>
+                  <th>Year</th>
+                  <th>Province</th>
+                  <th>Status</th>
+                  <th>Favorite</th>
+                </tr>
+              </thead>
+              <tbody>
+                {aggregatedData[selectedBrand].models[selectedModel].cars.map((car, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>
+                        <img src={car.Img100} alt="Car" className="car-image" onError={(e) => { e.target.onerror = null; e.target.src = 'fallback-image-url.jpg'; }} />
+                      </td>
+                      <td>{car.Prc}</td>
+                      <td>{car.Yr}</td>
+                      <td>{car.Province}</td>
+                      <td>{car.Status}</td>
+                      <td>
+                        <button className={`favorite-button ${isFavorite(car) ? 'gold' : ''}`} onClick={() => toggleFavorite(car)}>
+                          {isFavorite(car) ? '★' : '☆'}
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         )}
       </div>
